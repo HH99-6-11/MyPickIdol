9821e04ade52bf956b898c847ae5788b06c8287f
hyoungwonkang <hwplus@gmail.com>
2022-03-11 02:18:46 +0900

    test

--- prac_scraping.py
+++ prac_scraping.py
@@ -5,8 +5,7 @@ from time import sleep
 
 driver = webdriver.Chrome('./chromedriver')
 
-url = "https://www.vlive.tv/channel/FE619"
-driver.get(url)
+driver.get("https://www.vlive.tv/channel/FE619")
 sleep(3)
 
 req = driver.page_source
@@ -16,6 +15,6 @@ soup = BeautifulSoup(req, 'html.parser')
 # href가 들어있는 태그를 먼저 찾고, 그 위 부분의 공통태그를 찾음
 images = soup.select(".post_inner--2nG1X .link_post--1FLFt")
 
-# for image in images:
-#     href = image["href"]
-#     print(href)
+for image in images:
+    href = image["href"]
+    print(href)
